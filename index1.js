a649347f8807b5023a3fb198e0db9895f1ce3217
Aditya Narayan <connect.aditya.in@gmail.com>
2026-09-23 13:50:50 +0530

    let , var, const

---
+++ index1.js
@@ -0,0 +1,23 @@
+//variables and constant 
+
+const accountId = 14435 // cant be changd 
+let accountEmal = "aditya@google.com"
+
+let income;  //undefined 
+
+state = "ccu"
+
+console.log("Hello world")
+
+var a = 6;
+var b = 7; // we dont use var in the recent days now due to block scope issue we only use let and var
+
+console.log(a+b)
+
+console.log("hey there ")
+console.log(a-b)
+
+// accountId = 2 not allowed 
+accountEmal = "adi@gmail.com" //value updates here 
+
+console.log([accountEmal, state, income  ])

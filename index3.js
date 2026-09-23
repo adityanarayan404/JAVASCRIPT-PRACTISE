a260e80f9754605edc2a792ceae278e3dcbeb61c
Aditya Narayan <connect.aditya.in@gmail.com>
2026-09-23 14:36:24 +0530

    Conversion of datatypes

---
+++ index3.js
@@ -0,0 +1,22 @@
+// conversion and operations 
+
+let score = "33abc";
+ console.log(typeof score);
+
+ let valueInNumber = Number(score); 
+ console.log(typeof score);
+ console.log(valueInNumber); // NAN 
+
+ //true=> 1; false=>0
+
+ let isLoggedIn = 0 //1;
+ 
+ let booleanIsLoggedIn = Boolean(isLoggedIn); //boolean value of isLoggedIn
+
+ console.log(booleanIsLoggedIn);
+
+
+ let cibilScore = 400;
+ cibilScore = String (cibilScore); // changed the type from number to string
+ console.log(cibilScore);
+ console.log(typeof cibilScore);

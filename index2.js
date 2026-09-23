f40f4101e260ea208491f0b105076e08274fe94d
Aditya Narayan <connect.aditya.in@gmail.com>
2026-09-23 14:12:32 +0530

    Datatypes

---
+++ index2.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as new version 
+
+//alert(3+3)
+
+//number => 2 to the power 52
+//bigint
+//string => ""
+// boolean => true/false
+//null
+//undefined
+//symbol => unique
+
+//object
+console.log("aditya")
+
+console.log(typeof "ADITYA" );  
+let age = 18;
+console.log(typeof age );  
+
+console.log(typeof NULL);  //its an object 
+console.log(typeof undefined); //undefined 

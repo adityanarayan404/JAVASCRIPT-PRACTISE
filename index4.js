38bb1aae4ac9bd08b9fd4006586a09d12ae1cee0
Aditya Narayan <connect.aditya.in@gmail.com>
2026-09-23 15:41:29 +0530

    Comparison

---
+++ index4.js
@@ -0,0 +1,19 @@
+console.log(2>1);
+console.log(2>=2);
+console.log(2<2);
+console.log(2>=2);
+console.log(2<=2);
+
+
+//comparison coverts null to a number, treating it as 0 zero
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+
+// === strict equal => checks both value and datatype
+
+console.log("2" === 2); //false string and number 

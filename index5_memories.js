44d9f2a80b54b63be10c17842a750de5524b06cf
Aditya Narayan <connect.aditya.in@gmail.com>
2026-09-24 13:39:36 +0530

    Stack , Heap memory

---
+++ index5_memories.js
@@ -0,0 +1,25 @@
+// Stack (primitive ) COPY , Heap(non primitive)  REFERENCE 
+
+let herName  = "ayushi";
+let anothername = herName;
+
+anothername = "Aditya";
+
+
+console.log(herName);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi : "user@axis"
+
+}
+
+let userTwo = userOne;
+
+userTwo.email = "aditya@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
